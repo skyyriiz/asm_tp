88a1d8168d408da3d089a3fec7206508836c0493
skyyriiz <leblancmike525252@gmail.com>
2023-02-07 17:55:26 +0100

    asm01 done

--- asm01/asm01.s
+++ asm01/asm01.s
@@ -1,6 +1,21 @@
 global _start
 
+section .data
+    message db "13",10
+
 section .text
+    global _start
 
 _start:
+    ; afficher 1337
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, message
+    mov edx, 4
+    int 0x80
+
+    ; exit 0
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
     
---
+++ asm01/launch.sh
@@ -0,0 +1,6 @@
+# Get name of the folder for the compilation
+name="${PWD##*/}"
+
+nasm -f elf64 -o $name.o $name.s
+ld -s -o $name $name.o
+
